605f5fe4d62941425d49a605d1a0bdd46dd5f29d
yingchingluo <b20171016@gmail.com>
2024-08-14 15:30:35 +0800

    aside

---
+++ resources/views/components/aside.blade.php
@@ -0,0 +1,11 @@
+<ul>
+    <li><a href="#">基本資料</a></li>
+    <li>
+        <a href="#">老師</a>
+        <a href="#" style="font-size: x-small;">評分</a> 
+    </li>
+    <li>
+        <a href="#">學生</a>
+        <a href="#" style="font-size: x-small;">家長</a>
+    </li>
+</ul>
