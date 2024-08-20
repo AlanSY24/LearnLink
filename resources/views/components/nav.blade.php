b2050a2b854e35659b3b9836730d320d8f2cf370
luyenchen <ooppll0122@gmail.com>
2024-08-20 12:04:30 +0800

    改nav名稱

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,10 +5,10 @@
     </a>
     <button class="nav-ll-navbar-toggle" id="navLLToggle">☰</button>
     <ul class="nav-ll-navbar-menu" id="navLLMenu">
-        <li><a href="{{ route('teacher_list') }}">我是學生</a></li>
-        <li><a href="{{ route('student_case') }}">我是老師</a></li>
-        <li><a href="{{ route('findteacherPage') }}">發案</a></li>
-        <li><a href="{{ route('beteacher.create') }}">接案</a></li>
+        <li><a href="{{ route('teacher_list') }}">家教需求</a></li>
+        <li><a href="{{ route('student_case') }}">師資列表</a></li>
+        <li><a href="{{ route('findteacherPage') }}">尋找老師</a></li>
+        <li><a href="{{ route('beteacher.create') }}">教書育人</a></li>
         @auth
             <li>
                 <form action="{{ route('logout') }}" method="POST" class="nav-ll-logout-form">
